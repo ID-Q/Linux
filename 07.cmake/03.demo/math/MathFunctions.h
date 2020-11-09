cea19561cf3d533a54997eaeee1160ae9037b087
nick <nickdecodes@163.com>
2020-11-09 21:02:09 +0800

    03.demo

--- 07.cmake/03.demo/math/MathFunctions.h
+++ 07.cmake/03.demo/math/MathFunctions.h
@@ -1,5 +1,12 @@
-#ifndef POWER_H
-#define POWER_H
+/*************************************************************************
+	> File Name: MathFunctions.h
+	> Author: 
+	> Mail: 
+	> Created Time: 一 11/ 9 20:00:46 2020
+ ************************************************************************/
+
+#ifndef _MATHFUNCTIONS_H
+#define _MATHFUNCTIONS_H
 
 extern double power(double base, int exponent);
 
